b89aff0f7f5e7563f609b6778480d6abaf7d7845
Rakesh Raj <47710229+RakeshRaj97@users.noreply.github.com>
2021-05-04 20:03:35 +1000

    bump create-yolo-text-file.py
    program to create text file containing file paths of training and validation images referenced in "dota.data"

---
+++ utils/create-yolo-text-file.py
@@ -0,0 +1,16 @@
+# program to create text file containing file paths of training and validation images
+# this text files are used in "dota.data" 
+
+import os
+from tqdm import tqdm
+
+data_path = "path-to-dataset"
+
+def write_to_file(set='train'):
+  file = open(f'/fred/oz138/COS80028/P2/rakesh/yoltv4/{set}.txt', 'a+')
+  for i in tqdm(os.listdir(datapath + set + '/images')):
+    file.write(datapath + set + '/images' + f'{i}\n')
+    
+if __name__ == "__main__":
+  write_to_file('train')
+  write_to_file('val')
